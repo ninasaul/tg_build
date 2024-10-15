cfc50fc3ebfa4a434fffab11faabd72b30cbe7fd
saul <saul@a.com>
2024-10-15 14:54:17 +0800

    fix:init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
-import { AppRoot } from '@telegram-apps/telegram-ui';
-import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
+import { useLaunchParams, miniApp, useSignal } from "@telegram-apps/sdk-react";
+import { AppRoot } from "@telegram-apps/telegram-ui";
+// import { Navigate, Route, Routes, BrowserRouter } from "react-router-dom";
 
-import { routes } from '@/navigation/routes.tsx';
+// import { routes } from "@/navigation/routes.tsx";
 
 export function App() {
   const lp = useLaunchParams();
@@ -10,15 +10,18 @@ export function App() {
 
   return (
     <AppRoot
-      appearance={isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
+      appearance={isDark ? "dark" : "light"}
+      platform={["macos", "ios"].includes(lp.platform) ? "ios" : "base"}
     >
-      <HashRouter>
+      {/* <BrowserRouter>
         <Routes>
-          {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path="*" element={<Navigate to="/"/>}/>
+          {routes.map((route) => (
+            <Route key={route.path} {...route} />
+          ))}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter> */}
+      <div>App</div>
     </AppRoot>
   );
 }
