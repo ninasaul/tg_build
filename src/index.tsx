b006fd440744f3e3246769799e537e191a949b79
saul <saul@a.com>
2024-10-15 15:08:03 +0800

    fix:init

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-// import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { retrieveLaunchParams } from "@telegram-apps/sdk-react";
 
-import { Root } from '@/components/Root';
-// import { init } from '@/init.ts';
+import { Root } from "@/components/Root";
+import { init } from "@/init.ts";
 
-import '@telegram-apps/telegram-ui/dist/styles.css';
-import './index.css';
+import "@telegram-apps/telegram-ui/dist/styles.css";
+import "./index.css";
 
 // Mock the environment in case, we are outside Telegram.
-// import './mockEnv.ts';
+import "./mockEnv.ts";
 
 // // Configure all application dependencies.
-// init(retrieveLaunchParams().startParam === 'debug' || import.meta.env.DEV);
+init(retrieveLaunchParams().startParam === "debug" || import.meta.env.DEV);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Root/>
-  </StrictMode>,
+    <Root />
+  </StrictMode>
 );
